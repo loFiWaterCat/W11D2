4c912fb0f2c5b36c2cd5d1b43bf6011f0862907f
alan <alan001tk@gmail.com>
2022-08-23 14:02:19 -0700

    Finish coffee project

--- practice-for-week-14-react-context-review-with-tests-long-practice-main/src/App.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice-main/src/App.js
@@ -1,9 +1,13 @@
 import coffeeBeans from "./mockData/coffeeBeans.json";
+import SelectedCoffeeBean from "./components/SelectedCoffeeBean.js"
+import SetCoffeeBean from "./components/SetCoffeeBean";
 
 function App() {
   return (
     <>
-      <h1>Welcome to Coffee App</h1>
+    <h1>Welcome to Coffee App</h1>
+    <SelectedCoffeeBean/>
+    <SetCoffeeBean coffeeBeans={coffeeBeans}/>
     </>
   );
 }
--- practice-for-week-14-react-context-review-with-tests-long-practice-main/src/components/SetCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice-main/src/components/SetCoffeeBean.js
@@ -1,15 +1,25 @@
+import { useCoffeeContext } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const {coffeeBean, setCoffeeBeanId} = useCoffeeContext();
+
+  const handleChange = (e) => {
+    setCoffeeBeanId(e.target.value)
+  }
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
-      <select
+      <select 
         name="coffee-bean"
+        value={coffeeBean.id}
+        onChange={handleChange}
       >
         {coffeeBeans.map(bean => (
           <option
             key={bean.id}
             value={bean.id}
-          >
+            >
             {bean.name}
           </option>
         ))}
